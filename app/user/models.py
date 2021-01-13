a60f98d3c67102fe366d19f80b48cf7e853f5763
Saker <saker.lin@aif.tw>
2021-01-13 18:12:04 +0800

    Confirm that the token is correctly registered using the proper key

--- app/user/models.py
+++ app/user/models.py
@@ -1,12 +1,14 @@
 from app import db, bcrypt
+from itsdangerous import TimedJSONWebSignatureSerializer
+from itsdangerous import SignatureExpired, BadSignature
+from flask import current_app
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
     email = db.Column(db.String(320), unique=True)
     password = db.Column(db.String(100))
-    created_at = db.Column(db.DateTime, default=db.func.now())
-    updated_at = db.Column(db.DateTime, default=db.func.now())
+    confirm = db.Column(db.Boolean, default=False)
 
     @property
     def password(self):
@@ -21,3 +23,17 @@ class User(db.Model):
 
     def __repr__(self):
         return 'username:%s, email:%s' % (self.username, self.email)
+
+    def create_confirm_token(self, expires_in=3600):
+        s = TimedJSONWebSignatureSerializer(current_app.config['SECRET_KEY'], expires_in=expires_in)
+        return s.dumps({'user_id': self.id})
+
+    def validate_confirm_token(self, token):
+        s = TimedJSONWebSignatureSerializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return False
+        except BadSignature:
+            return False
+        return data
--- app/user/view.py
+++ app/user/view.py
@@ -3,6 +3,7 @@ from app import db
 from flask import render_template
 from app.user.models import User
 from app.user.form import FormRegister
+from app.sendmail import send_mail
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -16,13 +17,29 @@ def register():
         )
         db.session.add(user)
         db.session.commit()
+
+        token = user.create_confirm_token()
         
         send_mail(sender='Sender@domain.com',
                   recipients=['recipients@domain.com'],
                   subject='Activate your account',
                   template='author/mail/welcome',
                   mailtype='html',
-                  user=user)
+                  user=user,
+                  token=token)
 
         return 'Check Your Email and Activate Your Account'
     return render_template('user/register.html', form=form)
+
+@app.route('/user_confirm/<token>')
+def user_confirm(token):
+    user = User()
+    data = user.validate_confirm_token(token)
+    if data:
+        user = User.query.filter_by(id=data.get('user_id')).first()
+        user.confirm = True
+        db.session.add(user)
+        db.session.commit()
+        return 'Thands For Your Activate'
+    else:
+        return 'wrong token'
