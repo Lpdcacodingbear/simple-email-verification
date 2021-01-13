445f5450792b5764d192dd2dcb7cd9a97ca437b1
Saker <saker.lin@aif.tw>
2021-01-13 12:00:28 +0800

    Update user class schema

--- app/models.py
+++ app/models.py
@@ -4,9 +4,11 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-    phone = db.Column(db.String(25), unique=True)
+    username = db.Column(db.String(20))
     email = db.Column(db.String(320), unique=True)
     password = db.Column(db.String(100))
     created_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(db.DateTime, default=db.func.now())
+
+    def __repr__(self):
+        return 'username:%s, email:%s' % (self.username, self.email)
