487133eca9e7819b8888f33b8bd8a60528ba8c47
Saker <saker.lin@aif.tw>
2021-01-13 17:14:26 +0800

    Verify a password using BCrypt

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import Config
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_object(Config)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from app.user import view
--- app/user/models.py
+++ app/user/models.py
@@ -1,6 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from app import db, bcrypt
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,6 +7,17 @@ class User(db.Model):
     password = db.Column(db.String(100))
     created_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(db.DateTime, default=db.func.now())
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = Bcrypt.generate_password_hash(password).decode('utf8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return 'username:%s, email:%s' % (self.username, self.email)
